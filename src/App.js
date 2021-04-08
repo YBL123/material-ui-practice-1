957fd8e36e161198ae73ed3531fc6d1960dc39f3
Yarden Lawson <yardenlawson@gmail.com>
2021-04-08 20:42:13 +0100

    update map cards

--- src/App.js
+++ src/App.js
@@ -1,48 +1,109 @@
 import React from 'react';
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button } from '@material-ui/core';
-import { PhotoCamera } from '@material-ui/icons'
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+  Button,
+} from '@material-ui/core';
+import { PhotoCamera } from '@material-ui/icons';
 
+import useStyles from './styles';
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 const App = () => {
+  const classes = useStyles();
+
   return (
-      <>
-        <CssBaseline />
-        <AppBar position="relative">
-          <Toolbar>
-            <PhotoCamera />
-            <Typography variant="h6">
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera className={classes.icon} />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
               Photo Album
             </Typography>
-          </Toolbar>
-        </AppBar>
-        <main>
-          <div>
-            <Container maxWidth="sm">
-              <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
-                Photo Album
-              </Typography>
-              <Typography variant="h5" align="center" color="textSecondary" paragraph>
-                lorem ipsum dolor sit amet lorem lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit
-              </Typography>
-                <div>
-                  <Grid container spacing={2} justify="center">
-                    <Grid item>
-                      <Button>
-
-                      </Button>
-                    </Grid>
-                  </Grid>
-                </div>
-            </Container>
-          </div>
-        </main>
-      </>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              lorem ipsum dolor sit amet lorem lorem ipsum dolor sit lorem ipsum
+              dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum
+              dolor sit
+            </Typography>
+            <div className={classes.buttons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See My Photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary Action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+                          <Grid item key={card}>
+                          <Card className={classes.card}>
+                            <CardMedia 
+                              className={classes.cardMedia}
+                              image="https://cdn.mos.cms.futurecdn.net/9jhjfAEmyTkjjS9AL2aWJN.jpg"
+                              title="image title"
+                            />
+                            <CardContent className={classes.cardContent}>
+                              <Typography gutterBottom variant="h5">
+                                Heading
+                              </Typography>
+                              <Typography>
+                                This is a media card. You can use this section to describe the content.
+                              </Typography>
+                            </CardContent>
+                            <CardActions>
+                              <Button size="small" color="primary">View</Button>
+                              <Button size="small" color="primary">Edit</Button>
+                            </CardActions>
+                          </Card>
+                        </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+    </>
   );
 };
 
 export default App;
 
-// CssBaseline = predfined css 
+// CssBaseline = predfined css
 // AppBar = navbar
 // Typography = text, variant specifies it
 // gutterBottom = addes margin bottom
+// contained -> style -> coloured button
+// have to give images height in order to display them
---
+++ src/styles.js
@@ -0,0 +1,33 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+//call back function that immediately returns an object with all the styles
+// can explore theme on the material ui website
+const useStyles = makeStyles((theme) => ({
+  container: {
+    // inbuilt color
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  icon: {
+    marginRight: '20px',
+  },
+  buttons: {
+    marginTop: '40px',
+  },
+  cardGrid: {
+    padding: '20px 0'
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  cardMedia: {
+    paddingTop: '56.26%'  // 16:9
+  },
+  cardContent: {
+    flexGrow: 1, //expand fully
+  },
+}));
+
+export default useStyles;
